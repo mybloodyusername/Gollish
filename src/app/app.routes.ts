c29df534a2be74bcd1f7a56064ae2ad10619b6ff
mybloodyusername <y.sanaei94@gmail.com>
2026-09-25 23:41:03 +0330

    refactor: somewhere

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./components/home/home').then((c) => c.Home),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
+];
