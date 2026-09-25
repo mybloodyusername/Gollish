df13f61b681223799761dd65700c20cbad6d4f21
mybloodyusername <y.sanaei94@gmail.com>
2026-09-26 02:10:54 +0330

    feat: sounds and cat

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,4 +1,5 @@
-import { AfterViewInit, Component, ElementRef, viewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, signal, viewChild } from '@angular/core';
+import { fromEvent } from 'rxjs';
 
 @Component({
   imports: [],
@@ -7,5 +8,20 @@ import { AfterViewInit, Component, ElementRef, viewChild } from '@angular/core';
   templateUrl: './home.html',
 })
 export class Home implements AfterViewInit {
-  ngAfterViewInit(): void {}
+  /** Eyes squint shut while the cat is pressed/hovered. */
+  protected readonly eyesClosed = signal<boolean>(false);
+  protected readonly audio = new Audio('/public/purr.mp3');
+
+  constructor() {}
+
+  ngAfterViewInit() {}
+
+  protected purr() {
+    this.eyesClosed.set(true);
+    this.audio.play();
+  }
+
+  protected unPurr() {
+    this.eyesClosed.set(false);
+  }
 }
